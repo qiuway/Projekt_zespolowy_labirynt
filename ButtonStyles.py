3770914115ec67b867dac25c02eebeb3d7f1f53c
Dorovvsky <dorofilip@wp.pl>
2026-03-29 22:21:11 +0200

    Button Styles oddzielny plik Maze screen ma teraz siatkę i nawet można zmienić jej wielkość

---
+++ ButtonStyles.py
@@ -0,0 +1,95 @@
+#BASE_SCREEN_BUTTON
+BUTTON_ZERO = {
+    "font": ("Arial", 28, "bold"),
+    "bg": "#e9e9e9",
+    "fg": "black"
+}
+
+#MAZE_SCREEN_BUTTONS
+BUTTON_ONE = {
+    "font": ("Arial", 14),
+    "bg": "white",
+    "fg": "black",
+    "relief": "solid",
+    "bd": 3,
+    "width": 16,
+    "height": 2
+    }
+
+BUTTON_TWO = {
+    "font": ("Arial", 14),
+    "bg": "white",
+    "relief": "solid",
+    "bd": 3,
+    "width": 16,
+    "height": 3
+}
+
+BUTTON_THREE = {
+    "font": ("Arial", 14),
+    "bg": "white",
+    "fg": "black",
+    "relief": "solid",
+    "bd": 3,
+    "width": 16,
+}
+
+BUTTON_FOUR = {
+    "font": ("Arial", 14),
+    "bg": "white",
+    "fg": "black",
+    "selectcolor": "#dcdcdc",
+    "relief": "solid",
+    "bd": 3,
+    "width": 12,
+    "pady": 8
+}
+
+#MENU_SCREEN_BUTTONS
+BUTTON_FIVE = {
+    "font": ("Arial", 20),
+    "bg": "white",
+    "fg": "black",
+    "relief": "solid",
+    "bd": 3,
+    "width": 20,
+    "height": 2
+}
+
+#SETTINGS_SCREEN_BUTTONS i CREDITS_SCREEN_BUTTONS
+BUTTON_SIX = {
+    "font": ("Arial", 16),
+    "bg": "white",
+    "fg": "black",
+    "relief": "solid",
+    "bd": 3,
+    "width": 16,
+    "height": 2
+}
+
+BUTTON_SEVEN = {
+    "font": ("Arial", 18),
+    "bg": "white"
+}
+
+BUTTON_EIGHT = {
+    "bg": "white",
+    "relief": "solid",
+    "bd": 3,
+    "padx": 30,
+    "pady": 30
+}
+
+BUTTON_NINE = {
+    "bg": "white",
+    "relief": "solid",
+    "bd": 3,
+    "padx": 20,
+    "pady": 20
+}
+
+BUTTON_TEN = {
+    "font": ("Arial", 14),
+    "bg": "white",
+    "fg": "gray"
+}
